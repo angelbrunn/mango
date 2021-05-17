10f7ea9d08a7aea1d28d41fdc036d13610135af3
Angel Brun <angel.brun@naranjax.com>
2021-05-16 22:11:46 -0300

    wip - exam mango - component range

--- src/components/range/Range.jsx
+++ src/components/range/Range.jsx
@@ -1,5 +1,101 @@
-import React from 'react';
+import React, { useCallback, useEffect, useState, useRef } from 'react';
+import PropTypes from 'prop-types';
 
-export const Range = () => {
-    return <div className="container" />;
+export const Range = ({ min, max, fixed }) => {
+    const [minVal, setMinVal] = useState(min);
+    const [maxVal, setMaxVal] = useState(max);
+    const minValRef = useRef(min);
+    const maxValRef = useRef(max);
+    const range = useRef(null);
+
+    // Convert to percentage
+    const getPercent = useCallback(
+        value => Math.round(((value - min) / (max - min)) * 100),
+        [min, max]
+    );
+
+    // Set width of the range to decrease from the left side
+    useEffect(() => {
+        const minPercent = getPercent(minVal);
+        const maxPercent = getPercent(maxValRef.current);
+
+        if (range.current) {
+            range.current.style.left = `${minPercent}%`;
+            range.current.style.width = `${maxPercent - minPercent}%`;
+        }
+    }, [minVal, getPercent]);
+
+    // Set width of the range to decrease from the right side
+    useEffect(() => {
+        const minPercent = getPercent(minValRef.current);
+        const maxPercent = getPercent(maxVal);
+
+        if (range.current) {
+            range.current.style.width = `${maxPercent - minPercent}%`;
+        }
+    }, [maxVal, getPercent]);
+
+    return (
+        <div className="rangeConteiner">
+            <div
+                className="minValue"
+                id="editMinValue"
+                contentEditable
+                // onClick={event => console.log(event.target)}
+            >
+                {minVal}
+                <div className="amount">€</div>
+            </div>
+            <div className="container">
+                <input
+                    type="range"
+                    min={min}
+                    max={max}
+                    value={minVal}
+                    onChange={event => {
+                        const value = Math.min(
+                            Number(event.target.value),
+                            maxVal - 1
+                        );
+                        setMinVal(value);
+                        minValRef.current = value;
+                    }}
+                    className="thumb thumb--left"
+                    style={{ zIndex: minVal > max - 100 && '5' }}
+                />
+                <input
+                    type="range"
+                    min={min}
+                    max={max}
+                    value={maxVal}
+                    onChange={event => {
+                        const value = Math.max(
+                            Number(event.target.value),
+                            minVal + 1
+                        );
+                        setMaxVal(value);
+                        maxValRef.current = value;
+                    }}
+                    className="thumb thumb--right"
+                />
+
+                <div className="slider">
+                    <div className="slider__track" />
+                    <div ref={range} className="slider__range" />
+                    {/* <div className="slider__left-value minVal">{minVal}</div> */}
+                    {/* <div className="slider__right-value maxVal">{maxVal}</div> */}
+                </div>
+            </div>
+            <div className="maxValue">
+                {maxVal}
+                <div className="amount">€</div>
+            </div>
+        </div>
+    );
+};
+
+Range.propTypes = {
+    min: PropTypes.number.isRequired,
+    max: PropTypes.number.isRequired,
+    fixed: PropTypes.bool.isRequired
 };
--- src/views/home/Home.jsx
+++ src/views/home/Home.jsx
@@ -1,9 +1,13 @@
 import React from 'react';
+import { Range } from '../../components/range/Range';
 
 export const Home = () => {
+
+    // info - get data mockeable . . . .
     return (
         <>
             <div>Aca una desc del range</div>
+            <Range min={0} max={1000} fixed={false} />
         </>
     );
 };
