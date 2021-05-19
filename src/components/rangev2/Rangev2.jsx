80d6f7e03e53def2f54d72e580a45752f92bcb27
Angel Brun <angel.brun@naranjax.com>
2021-05-18 21:49:04 -0300

    wip-range without range

--- src/components/rangev2/Rangev2.jsx
+++ src/components/rangev2/Rangev2.jsx
@@ -19,9 +19,13 @@ export const Rangev2 = ({ min, max, fixed }) => {
 
         let divYYY = document.getElementById('yyy');
         let divCCC = document.getElementById('ccc');
-        let divCCC2 = document.getElementById('ccc2');
         let divLLL = document.getElementById('lll');
         let divNNN = document.getElementById('nnn');
+
+        let divYYY2 = document.getElementById('yyy2');
+        let divCCC2 = document.getElementById('ccc2');
+        let divLLL2 = document.getElementById('lll2');
+        let divNNN2 = document.getElementById('nnn2');
 
         // info - 240
         divCCC.style.left = '188px';
@@ -57,19 +61,19 @@ export const Rangev2 = ({ min, max, fixed }) => {
                       left = en.clientX;
 
                       console.log('#Go', left);
+                      console.log('#offset', divNNN.offsetLeft);
 
                       // info - cross nro one
-                      if (left >= 188 && left < 428) {
-                          divCCC.style.left = left - divNNN.offsetLeft + 'px';
-                          divLLL.style.left = left + 'px';
-                      }
-                      // info - cross nro two
-                      if (left > 428 && left <= 668) {
-                          divCCC2.style.left = left - divNNN.offsetLeft + 'px';
+                      if (left >= 188) {
+                          divCCC.style.left = left + 'px';
                           divLLL.style.left = left + 'px';
                       }
 
-                      //   divCCC.style.left = left + 'px';
+                      if (left <= 188) {
+                          divCCC.style.left = '188px';
+                          divLLL.style.left = left + 'px';
+                      }
+
                       divLLL.style.left = left + 'px';
                   },
                   false
@@ -96,28 +100,114 @@ export const Rangev2 = ({ min, max, fixed }) => {
                       console.log('#Back', left);
 
                       // info - cross nro one
-                      if (left >= 290 && left < 428) {
-                          divCCC.style.left = left - divNNN.offsetLeft + 'px';
+                      if (left >= 290) {
+                          divCCC.style.left = left + 'px';
                           divLLL.style.left = left + 'px';
-                      }else{
+                      } else {
                           divCCC.style.left = '188px';
                       }
-                      // info - cross nro two
-                      if (left > 428 && left <= 668) {
-                          divCCC2.style.left = left - divNNN.offsetLeft + 'px';
+
+                      if (left <= 188) {
+                          divCCC.style.left = '188px';
                           divLLL.style.left = left + 'px';
                       }
 
-                      //   if (left <= 668) {
-                      //       divCCC.style.left = left - divNNN.offsetLeft + 'px';
-                      //       divCCC2.style.left = left - divNNN.offsetLeft + 'px';
-                      //       divLLL.style.left = left + 'px';
-                      //   }
-                      //   divCCC.style.left = left + 'px';
                       divLLL.style.left = left + 'px';
-                      //   let v = Math.round(
-                      //       ((left - divNNN.offsetLeft) * 100) / 540
-                      //   );
+                  },
+                  false
+              )
+            : false;
+
+        // two
+
+        divYYY2 != null
+            ? divYYY2.addEventListener(
+                  'mousedown',
+                  () => {
+                      divYYY2.style.display = 'none';
+                  },
+                  false
+              )
+            : false;
+
+        divNNN2 != null
+            ? divNNN2.addEventListener(
+                  'mouseup',
+                  () => {
+                      divYYY2.style.display = 'block';
+                  },
+                  false
+              )
+            : false;
+
+        divNNN2 != null
+            ? divNNN2.addEventListener(
+                  'mousemove',
+                  en => {
+                      divLLL2.style.color = '#55a';
+                      left = en.clientX;
+
+                      console.log('#Go', left);
+                      console.log('#offset', divNNN2.offsetLeft);
+
+                      // info - cross nro two
+                      if (left <= 668 && left >= 210) {
+                          divCCC2.style.left = left + 'px';
+                          divLLL.style.left = left + 'px';
+                      }
+
+                      if (left >= 668) {
+                          divCCC2.style.left = '688px';
+                          divLLL2.style.left = left + 'px';
+                      }
+
+                      if (left === 210) {
+                          divCCC2.style.left = '210px';
+                          divLLL2.style.left = left + 'px';
+                      }
+
+                      divLLL2.style.left = left + 'px';
+                  },
+                  false
+              )
+            : false;
+
+        divNNN2 != null
+            ? divNNN2.addEventListener(
+                  'mouseout',
+                  () => {
+                      divYYY2.style.display = 'block';
+                  },
+                  false
+              )
+            : false;
+
+        divNNN2 != null
+            ? divNNN2.addEventListener(
+                  'mouseover',
+                  en => {
+                      divLLL2.style.color = '#55a';
+                      left = en.clientX;
+
+                      console.log('#Back', left);
+
+                      // info - cross nro two
+                      if (left <= 668) {
+                          divCCC2.style.left = left + 'px';
+                          divLLL.style.left = left + 'px';
+                      }
+
+                      if (left >= 668) {
+                          divCCC2.style.left = '688px';
+                          divLLL2.style.left = left + 'px';
+                      }
+
+                      if (left === 210) {
+                          divCCC2.style.left = '210px';
+                          divLLL2.style.left = left + 'px';
+                      }
+
+                      divLLL2.style.left = left + 'px';
                   },
                   false
               )
@@ -210,10 +300,16 @@ export const Rangev2 = ({ min, max, fixed }) => {
 
             <div id="sss" className="example_path"></div>
             <div id="ccc" className="example_block"></div>
-            <div id="ccc2" className="example_block"></div>
+            {/* <div id="ccc2" className="example_block"></div> */}
             <div id="lll" className="example_text"></div>
             <div id="nnn" className="touch"></div>
             <div id="yyy" className="overlay"></div>
+
+            <div id="sss2" className="example_path_2"></div>
+            <div id="ccc2" className="example_block_2"></div>
+            <div id="lll2" className="example_text_2"></div>
+            <div id="nnn2" className="touch_2"></div>
+            <div id="yyy2" className="overlay_2"></div>
 
             {/* <div
                     id="ballMax"
